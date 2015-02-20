3b30a318a6cf40bcbc1da98e1558b666876f7578
Tyraan <tomkly000@gmail.com>
2015-02-20 12:36:08 +0800

    Revert "fail commit"
    This reverts commit 6010877a14e9357610ac7b9a7a499753bc2d7b1c.

--- procat/views.py
+++ procat/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render,render_to_response
 from django.core.context_processors import request
 from django.http import HttpResponse
-
-
-
 def addC(request):
-    request
-    
+    request 
         
