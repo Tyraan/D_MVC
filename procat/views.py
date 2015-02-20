6010877a14e9357610ac7b9a7a499753bc2d7b1c
Tyraan <tomkly000@gmail.com>
2015-02-20 12:36:05 +0800

    fail commit
    fail commit

--- procat/views.py
+++ procat/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,render_to_response
 from django.core.context_processors import request
 from django.http import HttpResponse
+
+
+
 def addC(request):
-    request 
+    request
+    
         
